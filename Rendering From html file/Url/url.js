7ff38a7b86992db6dff7175bcf5ba0b123b4488c
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2021-03-17 00:27:55 +0500

    Create url.js

---
+++ Rendering From html file/Url/url.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var url = require('url');
+
+// localhost:8080?year=2019&month=5
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  console.log('before ', req.url);
+  console.log("url" , req.url);
+console.log("Method", req.method);
+  var q = url.parse(req.url, true).query;
+  console.log('after ', req.url);
+  console.log('q ', q);
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8089);
