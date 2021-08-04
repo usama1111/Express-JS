ba88eff03d83d8e0861d5e0bd68327a3cb4246fa
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2021-08-04 05:47:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 
 
 const app = express();
-const url = 'mongodb://usama:usama123@cluster0-shard-00-00.n0rad.mongodb.net:27017,cluster0-shard-00-01.n0rad.mongodb.net:27017,cluster0-shard-00-02.n0rad.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-104djj-shard-0&authSource=admin&retryWrites=true&w=majority'
+const url = 'mongodb://clustername and password@cluster0-shard-00-00.n0rad.mongodb.net:27017,cluster0-shard-00-01.n0rad.mongodb.net:27017,cluster0-shard-00-02.n0rad.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-104djj-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 
 app.use(bodyParser.json({extended: true }));
